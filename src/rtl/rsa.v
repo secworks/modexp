dd2c73f5af036575f21c1f43115cad13e5d655fb
Joachim Strömbergson <joachim@secworks.se>
2014-12-01 20:37:12 +0100

    Updated version info.

--- src/rtl/rsa.v
+++ src/rtl/rsa.v
@@ -67,7 +67,7 @@ module rsa(
   parameter ADDR_MODULUS_END   = 8'hff;
 
   parameter CORE_NAME0         = 32'h72736120; // "rsa "
-  parameter CORE_NAME1         = 32'h34303936; // "4096"
+  parameter CORE_NAME1         = 32'h38313932; // "8192"
   parameter CORE_VERSION       = 32'h302e3031; // "0.01"
 
   parameter DEFAULT_KEYSIZE    = 4'h4;
