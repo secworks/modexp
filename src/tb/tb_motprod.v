50399571131272312aa37458ef1f4c99c9af95c5
Joachim Strömbergson <joachim@secworks.se>
2015-03-27 14:42:14 +0100

    Added monitors from the RTL. Disabled all but the first failing test case.

--- src/tb/tb_motprod.v
+++ src/tb/tb_motprod.v
@@ -12,9 +12,13 @@ module tb_montprod();
 //----------------------------------------------------------------
 // Internal constant and parameter definitions.
 //----------------------------------------------------------------
-parameter DEBUG = 0;
-parameter CLK_HALF_PERIOD = 2;
-parameter CLK_PERIOD = 2 * CLK_HALF_PERIOD;
+  parameter SHOW_INIT = 0;
+
+  parameter DUMP_MEM = 0;
+  parameter DEBUG = 0;
+  parameter CLK_HALF_PERIOD = 2;
+  parameter CLK_PERIOD = 2 * CLK_HALF_PERIOD;
+
 
 //----------------------------------------------------------------
 // Register and Wire declarations.
@@ -39,6 +43,8 @@ reg [31 : 0] tb_a [0 : 255]; //tb_opa_data
 reg [31 : 0] tb_b [0 : 255]; //tb_opb_data reads here
 reg [31 : 0] tb_m [0 : 255]; //tb_opm_data reads here
 reg [31 : 0] tb_r [0 : 255]; //tb_result_data writes here
+
+  reg monitor_s;
 
 integer test_mont_prod_success;
 integer test_mont_prod_fail;
@@ -69,7 +75,9 @@ always @(posedge tb_clk)
     tb_opa_data <= tb_a[tb_opa_addr];
     tb_opb_data <= tb_b[tb_opb_addr];
     tb_opm_data <= tb_m[tb_opm_addr];
-    $display("a %x %x b %x %x m %x %x", tb_opa_addr, tb_a[tb_opa_addr], tb_opb_addr, tb_b[tb_opb_addr], tb_opm_addr, tb_m[tb_opm_addr]);
+
+    if (DUMP_MEM)
+      $display("a %x %x b %x %x m %x %x", tb_opa_addr, tb_a[tb_opa_addr], tb_opb_addr, tb_b[tb_opb_addr], tb_opm_addr, tb_m[tb_opm_addr]);
   end
 
 always @*
@@ -81,6 +89,7 @@ always @*
       end
   end
 
+
 //----------------------------------------------------------------
 // clk_gen
 //
@@ -90,6 +99,78 @@ always
   begin : clk_gen
     #CLK_HALF_PERIOD tb_clk = !tb_clk;
   end // clk_gen
+
+
+//----------------------------------------------------------------
+// S monitor
+//----------------------------------------------------------------
+  always @ (posedge tb_clk)
+    begin : s_monitor
+      if (monitor_s)
+        $display("S[ 0 ]: %x", dut.s_mem.mem[0] );
+    end
+
+
+
+//----------------------------------------------------------------
+// S monitor
+//----------------------------------------------------------------
+  always @ (posedge tb_clk)
+    begin : s_write_minitor
+      if (monitor_s)
+        if (dut.s_mem_we)
+          $display("Write to S[0x%02x]: 0x%08x", dut.s_mem_wr_addr, dut.s_mem_new);
+    end
+
+
+
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+  always @*
+    begin : bq_debug
+      $display("====================> B: %x Q: %x <=====================", dut.b_reg, dut.q_reg);
+    end
+
+      //case (montprod_ctrl_reg)
+      //  CTRL_LOOP_BQ:
+      //     $display("DEBUG: b: %d q: %d opa_data %x opb_data %x s_mem_read_data %x", b, q, opa_addr_reg, opa_data, opb_data, s_mem_read_data);
+      //  default:
+      //    begin end
+      //endcase
+
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+  always @ (posedge tb_clk)
+    begin : fsm_debug
+      if (dut.montprod_ctrl_we)
+        case (dut.montprod_ctrl_new)
+          dut.CTRL_IDLE:
+            $display("FSM: IDLE");
+          dut.CTRL_INIT_S:
+            $display("FSM: INIT_S");
+          dut.CTRL_LOOP_INIT:
+            $display("FSM: LOOP_INIT");
+          dut.CTRL_LOOP_ITER:
+            $display("FSM: LOOP_ITER");
+          dut.CTRL_LOOP_BQ:
+            $display("FSM: LOOP_BQ");
+          dut.CTRL_L_CALC_SM:
+            $display("FSM: LOOP_CALC_SM");
+          dut.CTRL_L_CALC_SA:
+            $display("FSM: LOOP_CALC_SA");
+          dut.CTRL_L_STALLPIPE_SA:
+            $display("FSM: STALL_PIPE");
+          dut.CTRL_L_CALC_SDIV2:
+            $display("FSM: LOOP_CALC_SDIV2");
+          dut.CTRL_EMIT_S:
+            $display("FSM: LOOP_EMIT_S");
+          dut.CTRL_DONE:
+            $display("FSM: DONE");
+          default:
+            $display("FSM: %x", dut.montprod_ctrl_new);
+        endcase
+    end
+
 
 //----------------------------------------------------------------
 // reset_dut()
@@ -118,6 +199,7 @@ task init_sim();
     tb_reset_n    = 0;
     tb_length     = 0;
     tb_calculate  = 0;
+    monitor_s = 1;
     test_mont_prod_success = 0;
     test_mont_prod_fail    = 0;
   end
@@ -176,6 +258,8 @@ task test_mont_prod(
     begin: copy_test_vectors
       integer i;
       integer j;
+
+      $display("*** Initializing...");
       for (i=32'h0; i<256; i=i+1)
         begin
           j = {i, 5'h0};
@@ -183,10 +267,12 @@ task test_mont_prod(
           tb_b[i] = b[j +: 32];
           tb_m[i] = m[j +: 32];
           tb_r[i] = 32'h0;
-          $display("*** init %0x: a: %x b: %x m: %x r: %x", i, tb_a[i], tb_b[i], tb_m[i], tb_r[i]);
+          if (SHOW_INIT)
+            $display("*** init %0x: a: %x b: %x m: %x r: %x", i, tb_a[i], tb_b[i], tb_m[i], tb_r[i]);
         end
     end
-    $display("*** test vector copied");
+
+    $display("*** Test vector copied");
     wait_ready();
     tb_length = length;
     signal_calculate();
@@ -201,7 +287,7 @@ task test_mont_prod(
       for (i=0; i<length; i=i+1)
         begin
           j = i * 32;
-          $display("offset: %d expected %x actual %x", i, expected[j +: 32], tb_r[i]);
+          $display("offset: %02d expected 0x%08x actual 0x%08x", i, expected[j +: 32], tb_r[i]);
           if (expected[j +: 32] != tb_r[i])
             begin
               success = 0;
@@ -224,14 +310,23 @@ initial
     $display("   -- Testbench for montprod started --");
     init_sim();
     reset_dut();
+
 //* A=  b B= 11 M= 13 A*B= 10 Ar=  9 Br=  7 Ar*Br=  1 A*B= 10
-    test_mont_prod( 1, {32'h9, 8160'h0}, {32'h7, 8160'h0}, {32'h13,8160'h0}, {32'h1,8160'h0} );
+
+//JS    test_mont_prod( 1, {32'h9, 8160'h0}, {32'h7, 8160'h0}, {32'h13,8160'h0}, {32'h1,8160'h0} );
+
 //* A=  b B= 13 M= 11 A*B=  5 Ar=  b Br=  2 Ar*Br=  5 A*B=  5
+
     test_mont_prod( 1, {32'hb, 8160'h0}, {32'h2, 8160'h0}, {32'h11,8160'h0}, {32'h5,8160'h0} );
+
 //* A= 11 B=  b M= 13 A*B= 10 Ar=  7 Br=  9 Ar*Br=  1 A*B= 10
-    test_mont_prod( 1, {32'h7, 8160'h0}, {32'h9, 8160'h0}, {32'h13,8160'h0}, {32'h1,8160'h0} );
+
+//JS    test_mont_prod( 1, {32'h7, 8160'h0}, {32'h9, 8160'h0}, {32'h13,8160'h0}, {32'h1,8160'h0} );
+
 //* A= 11 B= 13 M=  b A*B=  4 Ar=  2 Br=  a Ar*Br=  5 A*B=  4
-    test_mont_prod( 1, {32'h2, 8160'h0}, {32'ha, 8160'h0}, {32'h0b,8160'h0}, {32'h5,8160'h0} );
+
+//JS    test_mont_prod( 1, {32'h2, 8160'h0}, {32'ha, 8160'h0}, {32'h0b,8160'h0}, {32'h5,8160'h0} );
+
 //* A= 13 B=  b M= 11 A*B=  5 Ar=  2 Br=  b Ar*Br=  5 A*B=  5
 //* A= 13 B= 11 M=  b A*B=  4 Ar=  a Br=  2 Ar*Br=  5 A*B=  4
 //* A=10001 B= 11 M= 13 A*B=  7 Ar= 11 Br=  7 Ar*Br=  4 A*B=  7
