c7de8442d95c73d63aa5a7dd01d5ae564b90cdcc
Peter Magnusson <PeterMagnusson@users.noreply.github.com>
2015-04-22 23:16:49 +0200

    Generate 1 word test cases

--- src/testgenerator/src/org/crypttech/modexp/testgenerator/TestGeneratorBasic.java
+++ src/testgenerator/src/org/crypttech/modexp/testgenerator/TestGeneratorBasic.java
@@ -9,17 +9,26 @@ public class TestGeneratorBasic {
 	public static final List<TestVector> getTestVectors() {
 		Random rng = new Random(0); //any static seed
 		ArrayList<TestVector> list = new ArrayList<TestVector>();
+		
+		generateTestVectors(rng, list, 33, 2);
+		generateTestVectors(rng, list, 30, 1);
+		//generateTestVectors(rng, list, 32, 1); //will generate failing tests in C model 
+		//generateTestVectors(rng, list, 31, 1); //will generate failing tests in C model 
+		return list;
+	}
+
+	private static void generateTestVectors(Random rng,
+			ArrayList<TestVector> list, int bitLength, int wordLength) {
 		for(int i = 0; i < 10; i++) {
 			final long seed = rng.nextLong();
 			rng.setSeed(seed);
-			BigInteger m = BigInteger.probablePrime(33, rng);
-			BigInteger x = BigInteger.probablePrime(33, rng);
-			BigInteger e = BigInteger.probablePrime(33, rng);
+			BigInteger m = BigInteger.probablePrime(bitLength, rng);
+			BigInteger x = BigInteger.probablePrime(bitLength, rng);
+			BigInteger e = BigInteger.probablePrime(bitLength, rng);
 			BigInteger z = x.modPow(e, m);
-			TestVector tv = Util.generateTestVector("BASIC", Long.toString(seed), 2, m, x, e, z);
+			TestVector tv = Util.generateTestVector("BASIC", Long.toString(seed), wordLength, m, x, e, z);
 			list.add(tv);
 		}
-		return list;
 	}
 
 
