8862e8032df27202bccda1e0b3991d793032efe8
Peter Magnusson <PeterMagnusson@users.noreply.github.com>
2015-04-23 22:19:17 +0200

    Clean up code generation and add verilog support

--- src/testgenerator/src/org/crypttech/modexp/testgenerator/format/ModExpTestFormater.java
+++ src/testgenerator/src/org/crypttech/modexp/testgenerator/format/ModExpTestFormater.java
@@ -5,22 +5,33 @@ import java.io.PrintWriter;
 import org.crypttech.modexp.testgenerator.TestVector;
 
 public abstract class ModExpTestFormater implements AutoCloseable {
-	private PrintWriter pw;
-	
-	public ModExpTestFormater(PrintWriter pw) {
+	private static final char LF = (char) 10;
+	private final boolean alwaysLF;
+	private final PrintWriter pw;
+
+	public ModExpTestFormater(PrintWriter pw, boolean alwaysLF) {
 		this.pw = pw;
+		this.alwaysLF = alwaysLF;
 	}
-	
+
+	public ModExpTestFormater(PrintWriter pw) {
+		this(pw, false);
+	}
+
 	public abstract void format(TestVector testVector);
 
 	protected final void out(String s) {
 		pw.print(s);
+		if (alwaysLF)
+			pw.print(LF);
 	}
-	
+
+	protected final void out(String frmt, Object... args) {
+		out(String.format(frmt, args));
+	}
+
 	@Override
 	public void close() throws Exception {
 		pw.close();
 	}
-	
-	
 }
